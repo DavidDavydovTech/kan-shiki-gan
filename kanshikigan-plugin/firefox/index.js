46df438c2a342fb2c1f00f9f8e012e4aaaeb30f8
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 06:05:24 -0500

    Add arrive to the plugin library

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -1,51 +1,44 @@
 'use strict';
-(function () {
+(() => {
+  console.log('Now, how about drums?')
+  console.log('Base Drums!')
   const settings = {
     minimizeConnectionless: true,
   };
+
+  document.arrive('.jobs-details__main-content', (el) => {
+    console.log('Worked')
+    console.log('Arrive:', el)
+  });
+  console.log('Snare Drums!');
+  // const jobListElement = document.getElementsByClassName('jobs-search-results__list')[0];
+  // if (jobListElement === undefined || jobListElement === null) console.error("COULD NOT GET JOB LIST");
+
+  // const jobDetailParentElement = document.getElementsByClassName('jobs-search__right-rail')[0];
+  // if (jobDetailElement === undefined || jobDetailElement === null) console.error("COULD NOT GET JOB DETAIL");
+
+  // jobList.observe(jobListElement, { childList: true });
+  // jobDetail.observe(jobDetailElement, { childList: true });  
+
+  // const jobList = new MutationObserver((changes) => {
+  //   const {
+  //     minimizeConnectionless
+  //   } = settings;
   
-  const jobListElement = document.getElementsByClassName('jobs-search-results__list')[0];
-  if (jobListElement === undefined || jobListElement === null) console.error("COULD NOT GET JOB LIST");
-  const jobDetailElement = document.getElementsByClassName('jobs-search__right-rail')[0];
-  if (jobDetailElement === undefined || jobDetailElement === null) console.error("COULD NOT GET JOB DETAIL");
+  //   const newJobs = [];
+  //   for (let change of changes) {
+  //     if (change.addedNodes.length > 0) {
+  //       const { addedNodes } = change;
+  //       for (let element of addedNodes) {
+  //         if (element.nodeName === "LI") {
+  //           newJobs.push(element);
+  //         }
+  //       }
+  //     }
+  //   }
   
-  console.log('STARTED!')
-  window.addEventListener('load', function() {
-    console.log('WORKED!')
-    const jobListElement = document.getElementsByClassName('jobs-search-results__list')[0];
-    if (jobListElement === undefined || jobListElement === null) console.error("COULD NOT GET JOB LIST");
-    const jobDetailElement = document.getElementsByClassName('jobs-search__right-rail')[0];
-    if (jobDetailElement === undefined || jobDetailElement === null) console.error("COULD NOT GET JOB DETAIL");
-    
-    const jobList = new MutationObserver((changes) => {
-      const {
-        minimizeConnectionless
-      } = settings;
-    
-      const newJobs = [];
-      for (let change of changes) {
-        if (change.addedNodes.length > 0) {
-          const { addedNodes } = change;
-          for (let element of addedNodes) {
-            if (element.nodeName === "LI") {
-              newJobs.push(element);
-            }
-          }
-        }
-      }
-    
-      if (newJobs.length > 0) console.log("Found new jobs!~", newJobs);
-    });
-  
-    const jobDetail = new MutationObserver((changes) => {
-      const newJob = {
-        jobId: window.location.search,
-      }
-  
-      console.log(newJob);
-    });
-  
-    jobList.observe(jobListElement, { childList: true });
-    jobDetail.observe(jobDetailElement, { childList: true });  
-  })
+  //   if (newJobs.length > 0) console.log("Found new jobs!~", newJobs);
+  // });
+
+  // jobList.observe(jobListElement, { childList: true });
 })();
