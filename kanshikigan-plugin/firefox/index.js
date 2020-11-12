d0f3245680be64d2994183d46ae9f6971ebf5345
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 07:38:55 -0500

    Now gets the job description

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -10,10 +10,11 @@
     // the Job Details panel.
     document.arrive('.jobs-apply-button', (el) => {
       const newJob = {
-        title: null,
-        company: null,
         id: null,
-
+        title: null,
+        desc: null,
+        company: null,
+        companyId: null,
       }
       // The LinkedIn ID of a job.
       newJob.id = window
@@ -32,6 +33,23 @@
         .textContent
         .replace(/[ ]{2,}/gi, '')
         .replace(/[\n\t\r]{0,}/gi, '');
+      // Company ID
+      // newJob.id = document
+      //   .getElementsByClassName('jobs-details-top-card__job-title-link')[0]
+      //   .href
+      //   .match(/(?<=\?.{0,})(?<=currentJobId\=)[^=&]{0,}/gi)[0];
+      // Job Description obs-description-content__text
+      const descContainer = document
+        .getElementsByClassName('jobs-description-content__text')[0]
+        .childNodes;
+
+      for (let node of descContainer) {
+        console.log(node.nodeName)
+        if (node.nodeName === 'SPAN') {
+          newJob.desc = node.textContent;
+          break;
+        }
+      }
 
       console.log('New job:', newJob);
     });
