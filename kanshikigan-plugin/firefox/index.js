699068682bf6aa045a2c5a98b30a35a615e4ad56
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 06:31:16 -0500

    Find a reliable way of detectin job change

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -5,12 +5,15 @@
   const settings = {
     minimizeConnectionless: true,
   };
-  document.arrive('.jobs-details__main-content', (el) => {
-    console.log('Raw search:', window.location.search)
-    console.log('Current Job ID:', window.location.search.match(/(?<=\?.{0,})(currentJobId\=[^=&]{0,})/gi));
-    console.log('Job Details:', el);
-  });
-  document.arrive('.jobs-search-results__list-item', (el) => {
-    console.log('Job Listings:', el);
-  });
+  console.log('Current Job ID:', window.location.search.match(/(?<=\?.{0,})(?<=currentJobId\=)[^=&]{0,}/gi));
+  try {
+    document.arrive('.jobs-apply-button', (el) => {
+      console.log('Raw search:', window.location.search)
+      console.log('Current Job ID:', window.location.search.match(/(?<=\?.{0,})(?<=currentJobId\=)[^=&]{0,}/gi));
+      console.log('Job Details:', el);
+    });
+    document.arrive('.jobs-search-results__list-item', (el) => {
+      console.log('Job Listings:', el);
+    });
+  } catch (err) { console.error(err); }
 })();
