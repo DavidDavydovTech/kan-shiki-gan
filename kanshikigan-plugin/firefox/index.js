c2c74945686ce3c87c1c5d9a2765d7c44c395e56
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 03:25:08 -0500

    Add test code that tries to detect new jobs

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -1,1 +1,6 @@
 document.body.style.border = "5px solid red";
+let observer = new MutationObserver((...args) => {
+    console.log('ADDED NEW ELEMENTS!~')
+});
+const jobList = document.getElementsByClassName("jobs-search-results__list")
+observer.observe(jobList, {});
