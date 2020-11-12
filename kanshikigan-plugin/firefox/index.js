32164331c9235d804d4a6c0d2c8f45190a49d2e7
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 03:42:04 -0500

    Make plugin filter change objects and find jobs

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -2,8 +2,19 @@ document.body.style.border = "5px solid red";
 
 const jobList = document.getElementsByClassName("jobs-search-results__list")[0];
 if (jobList === undefined || jobList === null) console.error('COULD NOT GET JOB LIST')
+
 const observer = new MutationObserver((changes) => {
-    console.log('ADDED NEW ELEMENTS!~', changes)
+    const newJobs = [];
+    for (let change of changes) {
+        if (change.addedNodes.length > 0) {
+            const { addedNodes } = change;
+            for (let element of addedNodes) {
+                if ( element.nodeName === "LI" ) newJobs.push(element);
+            }
+        }
+    }
+
+    if (newJobs.length > 0) console.log('Found new jobs!~', newJobs);
 });
 console.log(jobList);
 observer.observe(jobList, {childList: true});
