16a14f1c56b967e1e47afd8feaa8d64adb059eb9
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 06:21:09 -0500

    Make the plugin capture the current jobId

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -1,12 +1,13 @@
 'use strict';
 (() => {
-  const state = {
-
-  }
+  let jobs = {};
+  
   const settings = {
     minimizeConnectionless: true,
   };
   document.arrive('.jobs-details__main-content', (el) => {
+    console.log('Raw search:', window.location.search)
+    console.log('Current Job ID:', window.location.search.match(/(?<=\?.{0,})(currentJobId\=[^=&]{0,})/gi));
     console.log('Job Details:', el);
   });
   document.arrive('.jobs-search-results__list-item', (el) => {
