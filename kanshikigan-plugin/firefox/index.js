1cd90a3d9174a39914dc1258cc3cd0a8e2473dac
David J. Davydov <davydovjd611@gmail.com>
2020-11-12 04:07:09 -0500

    Remove color changing

--- kanshikigan-plugin/firefox/index.js
+++ kanshikigan-plugin/firefox/index.js
@@ -1,27 +1,28 @@
 const settings = {
-    minimizeConnectionless: false,
+  minimizeConnectionless: true,
 };
 
 const jobList = document.getElementsByClassName("jobs-search-results__list")[0];
-if (jobList === undefined || jobList === null) console.error('COULD NOT GET JOB LIST')
+if (jobList === undefined || jobList === null) console.error("COULD NOT GET JOB LIST");
 
 const observer = new MutationObserver((changes) => {
-    const newJobs = [];
-    for (let change of changes) {
-        if (change.addedNodes.length > 0) {
-            const { addedNodes } = change;
-            for (let element of addedNodes) {
-                if ( element.nodeName === "LI" ) {
-                    console.log('Found a Job')
-                    if (minimizeConnectionless) element.style['background-color'] = '#CD5C5C !important';
-                    if (minimizeConnectionless) element.style.color = '#FFFFFF !important'; 
-                    newJobs.push(element);
-                }
-            }
-        }
-    }
+  const {
+    minimizeConnectionless
+  } = settings;
 
-    if (newJobs.length > 0) console.log('Found new jobs!~', newJobs);
+  const newJobs = [];
+  for (let change of changes) {
+    if (change.addedNodes.length > 0) {
+      const { addedNodes } = change;
+      for (let element of addedNodes) {
+        if (element.nodeName === "LI") {
+          newJobs.push(element);
+        }
+      }
+    }
+  }
+
+  if (newJobs.length > 0) console.log("Found new jobs!~", newJobs);
 });
 console.log(jobList);
-observer.observe(jobList, {childList: true});
+observer.observe(jobList, { childList: true });
